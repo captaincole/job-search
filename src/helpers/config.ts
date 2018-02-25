9ac0b3020742644a07afa1d7b58fae575e47fe23
Andrew <andrew.thielcole@gmail.com>
2018-02-24 18:19:16 -0800

    firebase

---
+++ src/helpers/config.ts
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyCLJhTKMVuQmFvgcfFW0PPsVUaGrRTX1GY",
+    authDomain: "brewsday-f1827.firebaseapp.com",
+    databaseURL: "https://brewsday-f1827.firebaseio.com",
+    projectId: "brewsday-f1827",
+    storageBucket: "brewsday-f1827.appspot.com",
+    messagingSenderId: "226192240816"
+}
